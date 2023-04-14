a7bfd49458709f7c0ccdc8d2732e96881ae93928
Roberta <federicoroberta1@gmail.com>
2023-04-14 11:53:59 +0200

    esercizio Promises lucky draw

--- file.mjs
+++
@@ -1,9 +0,0 @@
-import { writeFile } from "fs";
-import { Buffer } from "buffer";
-
-const data = new Uint8Array(Buffer.from("Hello Node.js"));
-// const data = "pippo";
-writeFile("message.txt", data, (err) => {
-  if (err) throw err;
-  console.log("The file has been saved!");
-});
