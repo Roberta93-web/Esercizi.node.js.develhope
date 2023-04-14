864d6dae1908f7b2e3cd22d25f3ba9375292856d
Roberta <federicoroberta1@gmail.com>
2023-04-14 12:02:28 +0200

    esercizio async luckydraw

--- esercizio.mjs
+++ esercizio.mjs
@@ -1,10 +1,3 @@
-Promises lucky draw
-
-The `luckyDraw` function returns a promise. Create a promise chain where the function is called for for each of the players: Joe, Caroline and Sabrina
-
-Log out the resolved value for each promise and handle any promise rejections in the chain.
-
-```
 function luckyDraw(player) {
   return new Promise((resolve, reject) => {
     const win = Boolean(Math.round(Math.random()));
@@ -18,4 +11,19 @@ function luckyDraw(player) {
     });
   });
 }
-```
+
+async function getResults() {
+  try {
+    const results = await Promise.all([
+      luckyDraw("Tina"),
+      luckyDraw("Jorge"),
+      luckyDraw("Julien"),
+    ]);
+
+    results.forEach((result) => console.log(result));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getResults();
