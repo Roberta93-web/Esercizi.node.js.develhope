7cfb4bce0fc8ff595f797b0b59c7130a9321a1a7
Roberta <federicoroberta1@gmail.com>
2023-04-20 13:05:45 +0200

    esercizio crud with dummy database

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import Joi from "joi";
+
+const app = express();
+const port = 4000;
+app.use(morgan("dev"));
+app.use(express.json());
+
+let planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  { id: 2, name: "Mars" },
+  {
+    id: 3,
+    name: "Jupiter",
+  },
+];
+
+//GET
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
+//GETByID
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+
+  res.status(200).json(planet);
+});
+
+//VALIDATION
+const planetSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  name: Joi.string().required(),
+});
+
+//POST
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+
+  res.status(201).json({ msg: "New Planet was created" });
+});
+
+//PUT
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  res.status(200).json({ msg: "New Planet was updated" });
+});
+
+//DELETE
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "New Planet was deleted" });
+});
+
+app.listen(port, () =>
+  console.log(`Server listening on port: http://localhost:${port}`)
+);
